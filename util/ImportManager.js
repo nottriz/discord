6d9f6f5a4e957db0a73e3f9159c739e0a3d700ae
Triz <kiebusiness1@gmail.com>
2021-05-25 01:48:46 +0100

    Create ImportManager.js

---
+++ util/ImportManager.js
@@ -0,0 +1,21 @@
+// Guild-Specific Event Files.
+import * as NIGHTSHADE_ALLEY from "../guilds/nightshade_alley/events.js";
+import * as CMCB_OFFICIAL from "../guilds/cmcb_official/events.js";
+import * as TOXICBUNCH from "../guilds/toxicbunch/events.js";
+import * as FINNARMY from "../guilds/finnarmy/events.js";
+import * as UNICORN_SHED from "../guilds/unicorn_shed/events.js";
+import * as BLITZYUK from "../guilds/blitzyuk/events.js";
+
+// Game Files.
+// TODO
+
+export const GuildManager = {
+    NIGHTSHADE_ALLEY,
+    CMCB_OFFICIAL,
+    TOXICBUNCH,
+    FINNARMY,
+    UNICORN_SHED,
+    BLITZYUK
+}
+
+export const GameManager = {}
